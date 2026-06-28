433110cd2a6ef6bd31c56f1a0e9a231e587ad41f
m-kazuha-dev <miyagikazuha@gmail.com>
2026-06-29 04:53:50 +0900

    feat: PHPとMySQLの接続環境を構築

--- src/index.php
+++ src/index.php
@@ -1,1 +1,12 @@
-<?php echo "Hello, Docker-Git World!" . "差分確認"; ?>
+<?php
+$host = 'db'; // docker-compose.ymlで付けたサービス名
+$db   = 'test_db';
+$user = 'root';
+$pass = 'root';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    echo "Connected!";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
